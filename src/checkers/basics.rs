d25693ebf41177b1c256caa2e85232bbb1087a4b
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-07-13 08:31:01 +0000

    Additions

--- src/checkers/basics.rs
+++ src/checkers/basics.rs
@@ -3,5 +3,24 @@ pub fn ReplaceClose(index: i32, string: &str, close: &str, replace: &str, head: 
         return head + tail;
     }
 
-    return head + ReplaceClose(next, tail, close, replace, head+replace, tail, string.find(close).unwrap() as i32)
+    return head + ReplaceClose(next, tail, close, replace, head+replace, tail, string.find(close).unwrap() as i32);
 }
+
+pub fn ClearBleed(index: i32, string: &str, open: &str, close: &str, replace: &str) -> String {
+    if (index < 0) {
+        return open + string + close;
+    }
+
+    return open + ReplaceClose(index, string, close, replace, "", "", string.find(open).unwrap() as i32);
+}
+
+pub fn FilterEmpty(open: &str, close: &str, replace: &str, at: i32) {
+    return fn (string: &str) -> String {
+        if (string == "" || string.len()) {
+            return "";
+        }
+
+        return ClearBleed(at, str, open, close, replace);
+    }
+}
+
---
+++ src/checkers/utils.rs
@@ -0,0 +1,3 @@
+pub fn StringElementExists(element: &str, array: [&str; 15]) -> bool {
+    // FOR LOOP
+}
